0d483e889cd78bf6f181106dcf77d1d04fee0d7d
Crowdbotics <engineering@crowdbotics.com>
2023-11-07 18:35:55 +0000

    Update store.

--- store/eventAPI/api.js
+++ store/eventAPI/api.js
@@ -1,72 +1,50 @@
-import axios from "axios";
+import axios from "axios"
 const eventAPI = axios.create({
   baseURL: "https://event-40036.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
-  return eventAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+  return eventAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
 }
-
 function api_v1_login_create(payload) {
-  return eventAPI.post(`/api/v1/login/`, payload.data);
+  return eventAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_signup_create(payload) {
-  return eventAPI.post(`/api/v1/signup/`, payload.data);
+  return eventAPI.post(`/api/v1/signup/`, payload)
 }
-
 function rest_auth_login_create(payload) {
-  return eventAPI.post(`/rest-auth/login/`, payload.data);
+  return eventAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return eventAPI.get(`/rest-auth/logout/`);
+  return eventAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return eventAPI.post(`/rest-auth/logout/`);
+  return eventAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return eventAPI.post(`/rest-auth/password/change/`, payload.data);
+  return eventAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return eventAPI.post(`/rest-auth/password/reset/`, payload.data);
+  return eventAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return eventAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
+  return eventAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return eventAPI.post(`/rest-auth/registration/`, payload.data);
+  return eventAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return eventAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
+  return eventAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return eventAPI.get(`/rest-auth/user/`);
+  return eventAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return eventAPI.put(`/rest-auth/user/`, payload.data);
+  return eventAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return eventAPI.patch(`/rest-auth/user/`, payload.data);
+  return eventAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -82,4 +60,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
